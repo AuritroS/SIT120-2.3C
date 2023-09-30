d0fa72c94750551fe731398baae57d1d21359fb0
Auritro <auritro.sami@gmail.com>
2023-09-30 18:10:28 +1000

    first commit

---
+++ src/components/HomePage.vue
@@ -0,0 +1,139 @@
+<template>
+  <div id="app">
+    <!------------------------------------------------------------------------------------------------->
+    <!-- 1. Template Syntax -->
+
+    <!-- a) Text Interpolation -->
+    <h1>{{ msg }}</h1>
+
+    <!-- b) Raw HTML (v-html) -->
+    <div v-html="paragraph"></div>
+    <div>{{ paragraph }}</div>
+
+    <!-- c) Attribute Bindings (v-bind:id) -->
+    <div :id="id">This element's ID is: {{ id }}</div>
+
+    <!-- d) JS Expressions Inside Syntax -->
+    <div>{{ check ? "True" : "False" }}</div>
+    <!------------------------------------------------------------------------------------------------->
+
+    <!------------------------------------------------------------------------------------------------->
+    <!-- 7. Event Handling [v-on:click] -->
+    <!-- a) Inline Handlers -->
+    <button v-on:click="incrementMe++">Increment Me: {{ incrementMe }}</button>
+
+    <!-- b) Method Handlers -->
+    <button @:click="handleClick">Click Me</button>
+    <!------------------------------------------------------------------------------------------------->
+
+    <!------------------------------------------------------------------------------------------------->
+    <!-- 6. List Rendering -->
+    <!-- a) v-for with an Object -->
+    <div v-for="value in myObject" :key="value">{{ value }}</div>
+
+    <!-- b) v-for with a Range -->
+    <span v-for="n in 10" :key="n">{{ n }}</span>
+
+    <!-- c) v-for on <template> -->
+    <div v-for="name in names" :key="name">
+      <p>{{ name }}</p>
+      <hr />
+    </div>
+
+    <!-- d) v-for with v-if -->
+    <div v-for="(name, i) in names" :key="i">
+      <p v-if="name === 'mario'">{{ name }}</p>
+    </div>
+
+    <!-- e) v-for with a Component -->
+    <!--                           -->
+    <!--                           -->
+    <!--                           -->
+
+    <!------------------------------------------------------------------------------------------------->
+
+    <!------------------------------------------------------------------------------------------------->
+    <!-- 8. Form Input Bindings -->
+    <!-- a & b). v-model & v-model modifiers -->
+    <input type="text" v-model.lazy="search" />
+    <div v-for="name in nameSearch" :key="name">{{ name }}</div>
+    <!------------------------------------------------------------------------------------------------->
+
+    <!------------------------------------------------------------------------------------------------->
+    <!-- 10. Components -->
+    <!-- a, b & c). Props, Events & Slots -->
+    <div :style="{ fontSize: postFontSize + 'em' }">
+      <TestComponent :tests="testValues" @enlarge-text="postFontSize += 0.1"
+        >Some Text</TestComponent
+      >
+    </div>
+    <!------------------------------------------------------------------------------------------------->
+
+    <!------------------------------------------------------------------------------------------------->
+    <!-- 11. Router -->
+    <router-link to="/about">About Page</router-link>
+    <!------------------------------------------------------------------------------------------------->
+  </div>
+</template>
+
+<script>
+import { ref, computed, watch, watchEffect } from "vue";
+import TestComponent from "../components/TestComponent.vue";
+export default {
+  name: "HomePage",
+  components: { TestComponent },
+  setup() {
+    // 3. Reactivity Fundamentals [ref(), <script setup>]
+
+    const msg = ref("Hello World!");
+    const postFontSize = ref(1);
+    const paragraph = ref("<p>Hello</p>");
+    const id = ref("1");
+    const check = ref(false);
+    const incrementMe = ref(0);
+    const search = ref("");
+    const myObject = ref({
+      title: "Harry Potter",
+      author: "J. K. Rowling",
+      publishedAt: "1997-06-26",
+    });
+    const names = ref(["bowser", "mario", "luigi", "yoshi", "toad", "peach"]);
+
+    // 2. Methods
+    const handleClick = () => {
+      alert("Clicked");
+    };
+
+    // 4. Computed Properties
+    const nameSearch = computed(() => {
+      return names.value.filter((name) => name.includes(search.value));
+    });
+
+    // 9. Watchers
+    watch(search, () => {
+      console.log("watching...");
+    });
+    watchEffect(() => {
+      console.log("Watch Effect Ran", search.value);
+    });
+
+    // 10. Components
+    const testValues = ref([
+      { title: "title1", body: "body1", id: 1 },
+      { title: "title2", body: "body2", id: 2 },
+    ]);
+    return {
+      msg,
+      paragraph,
+      id,
+      check,
+      incrementMe,
+      myObject,
+      handleClick,
+      nameSearch,
+      testValues,
+      postFontSize,
+    };
+  },
+};
+</script>
---
+++ src/components/TestComponent.vue
@@ -0,0 +1,15 @@
+<!-- 10. Components -->
+<template>
+  <div v-for="value in tests" :key="value.id">
+    <p>{{ value.title }}</p>
+    <p>{{ value.body }}</p>
+    <p><slot /></p>
+    <button @click="$emit('enlarge-text')">Enlarge Text</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["tests"],
+};
+</script>
